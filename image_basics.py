46a8db50d2a313e3139453be56bd1d56d0cafe03
lcomyn <113939233+lcomyn@users.noreply.github.com>
2024-09-26 16:03:17 +0200

    Update image_basics.py

--- image_basics.py
+++ image_basics.py
@@ -85,7 +85,7 @@ def register_images(img, label_img, atlas_img):
     # todo: apply the obtained transform to register the image (img) to the atlas image (atlas_img)
     # hint: 'Resample' (with referenceImage=atlas_img, transform=transform, interpolator=sitkLinear,
     # defaultPixelValue=0.0, outputPixelType=img.GetPixelIDValue())
-    registered_img = sitk.Resample(img, referenceImg = atlas_img, transform = transform, interpolator = sitk.sitkLinear, defaultPixelValue = 0.0,
+    registered_img = sitk.Resample(img, referenceImg = atlas_img, transform = transform, interpolator = sitkLinear, defaultPixelValue = 0.0,
                                    outputPixelType = img.GetPixelIDValue)
     # todo: modify here
 
@@ -93,7 +93,7 @@ def register_images(img, label_img, atlas_img):
     # be careful with the interpolator type for label images!
     # hint: 'Resample' (with interpolator=sitkNearestNeighbor, defaultPixelValue=0.0,
     # outputPixelType=label_img.GetPixelIDValue())
-    registered_label = sitk.Resample(img, referenceImg = atlas_img, transform = transform, interpolator = sitk.sitkNearestNeighbor, defaultPixelValue = 0.0,
+    registered_label = sitk.Resample(img, referenceImg = atlas_img, transform = transform, interpolator =sitkNearestNeighbor, defaultPixelValue = 0.0,
                                      outputPixelType = img.GetPixelIDValue)  # todo: modify here
 
     return registered_img, registered_label
